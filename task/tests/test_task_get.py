55c216479746f56f870ac61278c7c112f73d6a9f
montalvas <lucas_montalvani@hotmail.com>
2022-02-27 21:54:03 -0300

    checklist tasks

--- task/tests/test_task_get.py
+++ task/tests/test_task_get.py
@@ -1,10 +1,11 @@
 from django.urls import reverse
+from task.models import Task
 from pytest_django.asserts import assertContains
 import pytest
 
 
 @pytest.fixture
-def response(client):
+def response(client, db):
     resp = client.get(reverse('task:home'))
     return resp
 
@@ -19,3 +20,22 @@ def test_form_present(response):
 def test_form_button_submit(response):
     assertContains(response, '<button type="submit"')
     # Verifica se possui o botao de salvar
+
+@pytest.fixture
+def pending_task_list(db):
+    tasks = [
+        Task(name='Task 1', done=False),
+        Task(name='Task 2', done=False),
+    ]
+
+    Task.objects.bulk_create(tasks)
+    return tasks
+
+@pytest.fixture
+def response_task_list(client, pending_task_list):
+    resp = client.get(reverse('task:home'))
+    return resp
+
+def test_pending_task_list_present(response_task_list, pending_task_list):
+    for task in pending_task_list:
+        assertContains(response_task_list, task.name)
--- task/views.py
+++ task/views.py
@@ -1,16 +1,22 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
+
+from task.models import Task
 from .forms import NewTaskForm
 
 
 def home(request):
+    pending_tasks = Task.objects.all()
+    context = {'pending_tasks': pending_tasks}
+    
     if request.method == 'POST':
         form = NewTaskForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('task:home'))
         else:
-            return render(request, 'task/home.html', {'form': form}, status=400)
-
-    return render(request, 'task/home.html')
+            context.update({'form': form})
+            return render(request, 'task/home.html', context, status=400)
+    
+    return render(request, 'task/home.html', context)
